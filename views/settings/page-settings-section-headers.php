0351453d4a5386daa65e37a4546297c0e9f614b6
Alexander Knueppel <alexander.knueppel@gmx.net>
2015-02-20 09:34:27 +0100

    Add views

---
+++ views/settings/page-settings-section-headers.php
@@ -0,0 +1,9 @@
+<?php if ( 'fsh_section-basic' == $section['id'] ) : ?>
+
+	<p>Example section introduction.</p>
+
+<?php elseif ( 'fsh_section-advanced' == $section['id'] ) : ?>
+
+	<p>Another example section introduction.</p>
+
+<?php endif; ?>
